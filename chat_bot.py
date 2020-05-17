7e3c6ba0ffecc2e8b4f252c6e528b47dd9d060c1
leoska <leo77551@gmail.com>
2020-05-17 12:28:48 +0300

    push source of chat_bot

---
+++ chat_bot.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 11:56:20 2020
+
+@author: Leo77
+"""
+
+import telebot
+import random
+from telebot import types
+
+TOKEN = '' # PASTE HERE YOUR TOKEN
+ 
+bot = telebot.TeleBot(TOKEN)
+ 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('welcome.tgs', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+ 
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Случайное число")
+    item2 = types.KeyboardButton("Как дела?")
+ 
+    markup.add(item1, item2)
+ 
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный для мастер-класса РТУ \"МИРЭА\"".format(message.from_user, bot.get_me()),
+        parse_mode='html', reply_markup=markup)
+ 
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == 'Случайное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == 'Как дела?':
+ 
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+ 
+            markup.add(item1, item2)
+ 
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить :/')
+ 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Это же отлично!')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Не грусти!')
+ 
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?",
+                reply_markup=None)
+ 
+            # show alert
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                text="тестовое уведомление")
+ 
+    except Exception as e:
+        print(repr(e))
+ 
+# RUN
+bot.polling(none_stop=True, timeout=123)
